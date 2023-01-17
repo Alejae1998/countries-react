6e87797acb8ee4684d5ee43bc3fb5d2c2302be60
Alejae1998 <alejagr1998@gmail.com>
2023-01-17 15:52:42 -0800

    working on main.js conecting the app with Main. getting my h1 to display

---
+++ src/components/Main/Main.js
@@ -0,0 +1,12 @@
+// import { useCountries } from '../../hooks/useCountries.js';
+// import CountryCard from '../CountryCard/CountryCard.js';
+
+export default function Main() {
+  //   const { continent, setContinent } = useCountries();
+  return (
+    <main>
+      <h2>Flags of the world by: Alchemy codeLab</h2>
+      <div></div>
+    </main>
+  );
+}
