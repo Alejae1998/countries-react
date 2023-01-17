e0c5e6a909944eb333e47f87266257404c145801
Alejae1998 <alejagr1998@gmail.com>
2023-01-17 15:36:57 -0800

    building my countryCard inserting the images calling the API

---
+++ src/components/CountryCard/CountryCard.js
@@ -0,0 +1,14 @@
+export default function CountryCard({ name, iso2 }) {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <img
+        src={`https://flagcdn.com/96x72/${iso2}.png`}
+        srcSet={`https://flagcdn.com/192x144/${iso2}.png 2x`}
+        width="96"
+        height="72"
+        alt={`${name}`}
+      />
+    </div>
+  );
+}
